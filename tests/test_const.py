eefce0ffbc0b04301c5ea45d073d82d6c47db226
Frank Wickström <frwickst@gmail.com>
2022-01-29 14:43:09 +0200

    Add typing to const tests

--- tests/test_const.py
+++ tests/test_const.py
@@ -1,7 +1,7 @@
 from huum.const import STATUS_CODE_TEXTS, SaunaStatus
 
 
-def test_code_texts():
+def test_code_texts() -> None:
     status_enum_list = list(map(int, SaunaStatus))
     for status_code in status_enum_list:
         assert STATUS_CODE_TEXTS.get(status_code, False)
